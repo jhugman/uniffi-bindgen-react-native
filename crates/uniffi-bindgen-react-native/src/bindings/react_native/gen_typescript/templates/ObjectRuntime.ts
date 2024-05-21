eae62b87e2d59969dd75755a29f2ca2be58e12e0
James Hugman <james@hugman.tv>
2024-05-21 14:43:02 +0100

    TS: Objects (fixup)

--- crates/uniffi-bindgen-react-native/src/bindings/react_native/gen_typescript/templates/ObjectRuntime.ts
+++ crates/uniffi-bindgen-react-native/src/bindings/react_native/gen_typescript/templates/ObjectRuntime.ts
@@ -1,3 +1,4 @@
 {{- self.import_infra_type("UniffiObjectInterface", "objects") -}}
 {{- self.import_infra_type("UnsafeMutableRawPointer", "objects") -}}
 {{- self.import_infra_type("FfiConverter", "ffi-converters") -}}
+{{- self.import_infra_type("UniffiRustCallStatus", "rust-call") }}
