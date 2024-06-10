dc00db00e3cfdc24720a85bb9de64bc161218dd5
James Hugman <james@hugman.tv>
2024-06-10 14:44:43 +0100

    Find package.json to find the project root

--- crates/cli/src/workspace.rs
+++ crates/cli/src/workspace.rs
@@ -6,6 +6,15 @@
 use anyhow::Result;
 use camino::Utf8PathBuf;
 
+pub(crate) fn package_json() -> Result<Utf8PathBuf> {
+    let pwd = uniffi_common::pwd()?;
+
+    let package_json = uniffi_common::resolve(pwd, "package.json")?;
+    Ok(package_json.expect("Must be run under a directory containing a package.json file"))
+}
+
 pub(crate) fn project_root() -> Result<Utf8PathBuf> {
-    Ok(Utf8PathBuf::new())
+    let package_json = package_json()?;
+    let dir = package_json.parent().expect("Must be a directory");
+    Ok(dir.into())
 }
