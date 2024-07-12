24df86b68a2e01f6e31f374d1452f3de0941c8e8
James Hugman <james@hugman.tv>
2024-07-12 01:01:29 +0100

    Import the fixtures/futures-example from uniffi-rs

---
+++ fixtures/futures-example/tests/bindings/test_futures_example.ts
@@ -0,0 +1,23 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/
+ */
+import { sayAfter } from "../../generated/uniffi_example_futures";
+import { asyncTest } from "@/asserts";
+import { console } from "@/hermes";
+
+asyncTest("sayAfter once", async (t): Promise<void> => {
+  const result = await sayAfter(BigInt("500"), "World");
+  t.assertEqual(result, `Hello, World!`);
+  t.end();
+});
+
+asyncTest("sayAfter multiple times", async (t): Promise<void> => {
+  const strings = ["World", "Async", "Test", "Rust"];
+  for (const s of strings) {
+    const result = await sayAfter(BigInt("100"), s);
+    t.assertEqual(result, `Hello, ${s}!`);
+  }
+  t.end();
+});
