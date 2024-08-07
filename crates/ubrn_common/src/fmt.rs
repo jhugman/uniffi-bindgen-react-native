2ed991b949ad974f287b8e68fccfe4124c982603
James Hugman <james@hugman.tv>
2024-08-07 18:45:09 +0100

    Add check to xtask fmt

--- crates/ubrn_common/src/fmt.rs
+++ crates/ubrn_common/src/fmt.rs
@@ -10,15 +10,19 @@ use which::which;
 
 use crate::{file_paths, resolve};
 
-pub fn clang_format<P: AsRef<Utf8Path>>(path: P) -> Result<Option<Command>> {
+pub fn clang_format<P: AsRef<Utf8Path>>(path: P, check_only: bool) -> Result<Option<Command>> {
     if which("clang-format").is_err() {
         return Ok(None);
     }
 
     let path = path.as_ref();
     let mut cmd = Command::new("clang-format");
-    cmd.arg("-i")
-        .arg("--style=file")
+    if check_only {
+        cmd.arg("--dry-run").arg("--Werror");
+    } else {
+        cmd.arg("-i");
+    }
+    cmd.arg("--style=file")
         .arg("--fallback-style=LLVM")
         .args(file_paths(&format!("{path}/**/*.[ch]"))?)
         .args(file_paths(&format!("{path}/**/*.[ch]pp"))?)
@@ -26,11 +30,16 @@ pub fn clang_format<P: AsRef<Utf8Path>>(path: P) -> Result<Option<Command>> {
     Ok(Some(cmd))
 }
 
-pub fn prettier<P: AsRef<Utf8Path>>(out_dir: P) -> Result<Option<Command>> {
+pub fn prettier<P: AsRef<Utf8Path>>(out_dir: P, check_only: bool) -> Result<Option<Command>> {
     let prettier = resolve(&out_dir, "node_modules/.bin/prettier")?;
     Ok(if let Some(prettier) = prettier {
         let mut cmd = Command::new(prettier);
-        cmd.arg(".").arg("--write").current_dir(out_dir.as_ref());
+        if check_only {
+            cmd.arg("--check");
+        } else {
+            cmd.arg("--write");
+        }
+        cmd.arg(".").current_dir(out_dir.as_ref());
         Some(cmd)
     } else {
         None
