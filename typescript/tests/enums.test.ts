f00738dc6aa3cadf45867244c5fcd1986a4d37c1
James Hugman <james@hugman.tv>
2024-08-30 19:23:44 +0100

    Enums in playground

--- typescript/tests/enums.test.ts
+++ typescript/tests/enums.test.ts
@@ -11,17 +11,24 @@ test("Enums have private fields", (t) => {
   const v1: MyEnum = new MyEnum.Variant1({ myValue: "string" });
   const v2: MyEnum = new MyEnum.Variant2(42, "string");
 
-  function switchGetTag(v: MyEnum): string {
+  function switchGetTag(obj: any): string {
+    if (!MyEnum.instanceOf(obj)) {
+      t.fail("Obj should be a MyEnum");
+      return "";
+    }
+    const v = obj;
     switch (v.tag) {
       case "Variant1": {
         const { myValue } = v.inner;
         t.assertEqual(myValue, "string");
+        t.assertTrue(MyEnum.Variant1.instanceOf(v));
         return v.tag;
       }
       case "Variant2": {
         const [p1, p2] = v.inner;
         t.assertEqual(p1, 42);
         t.assertEqual(p2, "string");
+        t.assertTrue(MyEnum.Variant2.instanceOf(v));
         return v.tag;
       }
     }
