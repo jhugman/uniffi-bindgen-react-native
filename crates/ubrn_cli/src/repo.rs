478976f96193ef41b3015b5be3e754998c7d3a3a
Johannes Marbach <n0-0ne+github@mailbox.org>
2024-10-09 20:03:37 +0200

    Fix tag checkouts
    Fixes: #118

--- crates/ubrn_cli/src/repo.rs
+++ crates/ubrn_cli/src/repo.rs
@@ -79,21 +79,41 @@ impl GitRepoArgs {
             run_cmd(&mut cmd)?;
         }
 
-        // git fetch --depth 1 origin $branch
+        // git ls-remote origin $branch
+        let output = Command::new("git")
+            .current_dir(self.directory(project_root)?)
+            .arg("ls-remote")
+            .arg("origin")
+            .arg(&self.branch).output()?;
+        let output = String::from_utf8(output.stdout)?;
+
+        // Find $branch in the output and resolve the SHA or fall back to $branch
+        let mut sha = None;
+        let branch_ref = format!("refs/heads/{}", &self.branch);
+        let tag_ref = format!("refs/tags/{}", &self.branch);
+        for line in output.lines() {
+            if line.ends_with(&branch_ref) || line.ends_with(&tag_ref) {
+                sha = Some(line.split_whitespace().next().unwrap());
+                break;
+            }
+        }
+        let sha = sha.unwrap_or(&self.branch);
+
+        // git fetch --depth 1 origin $sha
         let mut cmd = Command::new("git");
         cmd.current_dir(self.directory(project_root)?)
             .arg("fetch")
             .arg("--depth")
             .arg("1")
             .arg("origin")
-            .arg(&self.branch);
+            .arg(sha);
         run_cmd(&mut cmd)?;
 
-        // git checkout $branch
+        // git checkout $sha
         let mut cmd = Command::new("git");
         cmd.current_dir(self.directory(project_root)?)
             .arg("checkout")
-            .arg(&self.branch);
+            .arg(sha);
         run_cmd(&mut cmd)
     }
 }
