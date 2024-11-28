d82eb2995c53cccd8f1547db997d29d13408d3f9
James Hugman <james@hugman.tv>
2024-11-28 19:50:25 +0000

    Refactored xtask run into wasm and jsi variants

---
+++ xtask/src/run/jsi.rs
@@ -0,0 +1,58 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/
+ */
+use super::{cpp_bindings::CppBindingArg, RunCmd};
+use crate::bootstrap::{Bootstrap, TestRunnerCmd};
+use anyhow::{Ok, Result};
+use camino::Utf8PathBuf;
+
+pub(crate) struct Jsi;
+
+impl Jsi {
+    pub(crate) fn run(&self, cmd: &RunCmd) -> Result<()> {
+        TestRunnerCmd.ensure_ready()?;
+        let so_file = self.prepare_library_path(cmd)?;
+        let js_file = cmd.js_file.prepare_for_jsi()?;
+        TestRunnerCmd.run(&js_file, so_file.as_ref())?;
+        Ok(())
+    }
+
+    fn prepare_library_path(&self, cmd: &RunCmd) -> Result<Option<Utf8PathBuf>> {
+        let switches = &cmd.switches;
+        let clean = cmd.clean;
+
+        match (&cmd.crate_, &cmd.cpp_binding, &cmd.generate_bindings) {
+            (Some(crate_), Some(cpp), _) => {
+                let crate_ = crate_.cargo_build(clean)?;
+                let target_dir = crate_.target_dir();
+                let lib_name = crate_.library_name();
+                let cpp = CppBindingArg::with_file(cpp.clone());
+                let so_file = cpp.compile_with_crate(clean, target_dir, lib_name)?;
+                Ok(Some(so_file))
+            }
+            (None, Some(cpp), _) => {
+                let cpp = CppBindingArg::with_file(cpp.clone());
+                let so_file = cpp.compile_without_crate(clean)?;
+                Ok(Some(so_file))
+            }
+            (Some(crate_), None, Some(bindings)) => {
+                let profile = crate_.profile();
+                let crate_ = crate_.cargo_build(clean)?;
+                let crate_lib = crate_.library_path(None, profile);
+                let target_dir = crate_.target_dir();
+                let lib_name = crate_.library_name();
+                let cpp_files = bindings.generate(
+                    &crate_lib,
+                    &crate_.manifest_path().to_path_buf(),
+                    switches,
+                )?;
+                let cpp = CppBindingArg::with_files(&cpp_files);
+                let so_file = cpp.compile_with_crate(clean, target_dir, lib_name)?;
+                Ok(Some(so_file))
+            }
+            (_, _, _) => Ok(None),
+        }
+    }
+}
--- xtask/src/run/mod.rs
+++ xtask/src/run/mod.rs
@@ -5,19 +5,22 @@
  */
 pub(crate) mod cpp_bindings;
 pub(crate) mod generate_bindings;
+pub(crate) mod jsi;
+pub(crate) mod nodejs;
 pub(crate) mod rust_crate;
 pub(crate) mod typescript;
+pub(crate) mod wasm;
 
-use anyhow::{Ok, Result};
+use anyhow::Result;
 use camino::Utf8PathBuf;
 use clap::Args;
 use generate_bindings::GenerateBindingsArg;
-use ubrn_bindgen::SwitchArgs;
-use ubrn_common::CrateMetadata;
+use jsi::Jsi;
+use nodejs::NodeJs;
+use ubrn_bindgen::{AbiFlavor, SwitchArgs};
+use wasm::Wasm;
 
-use crate::bootstrap::{Bootstrap, TestRunnerCmd};
-
-use self::{cpp_bindings::CppBindingArg, rust_crate::CrateArg, typescript::EntryArg};
+use self::{rust_crate::CrateArg, typescript::EntryArg};
 
 #[derive(Debug, Args)]
 pub(crate) struct RunCmd {
@@ -45,52 +48,9 @@ pub(crate) struct RunCmd {
 
 impl RunCmd {
     pub(crate) fn run(&self) -> Result<()> {
-        TestRunnerCmd.ensure_ready()?;
-        let so_file = self.prepare_library_path(&self.switches)?;
-
-        let js_file = self.js_file.prepare()?;
-        TestRunnerCmd.run(&js_file, so_file.as_ref())?;
-        Ok(())
-    }
-
-    fn prepare_library_path(&self, switches: &SwitchArgs) -> Result<Option<Utf8PathBuf>> {
-        let clean = self.clean;
-        let (release, info) = if let Some(crate_) = &self.crate_ {
-            (
-                CrateMetadata::profile(crate_.release),
-                Some(crate_.cargo_build(clean)?),
-            )
-        } else {
-            (CrateMetadata::profile(false), None)
-        };
-
-        match (&info, &self.cpp_binding, &self.generate_bindings) {
-            (Some(crate_), Some(cpp), _) => {
-                let target_dir = crate_.target_dir();
-                let lib_name = crate_.library_name();
-                let cpp = CppBindingArg::with_file(cpp.clone());
-                let so_file = cpp.compile_with_crate(clean, target_dir, lib_name)?;
-                Ok(Some(so_file))
-            }
-            (None, Some(cpp), _) => {
-                let cpp = CppBindingArg::with_file(cpp.clone());
-                let so_file = cpp.compile_without_crate(clean)?;
-                Ok(Some(so_file))
-            }
-            (Some(crate_), None, Some(bindings)) => {
-                let crate_lib = crate_.library_path(None, release);
-                let target_dir = crate_.target_dir();
-                let lib_name = crate_.library_name();
-                let cpp_files = bindings.generate(
-                    &crate_lib,
-                    &crate_.manifest_path().to_path_buf(),
-                    switches,
-                )?;
-                let cpp = CppBindingArg::with_files(&cpp_files);
-                let so_file = cpp.compile_with_crate(clean, target_dir, lib_name)?;
-                Ok(Some(so_file))
-            }
-            (_, _, _) => Ok(None),
+        match &self.switches.flavor {
+            AbiFlavor::Jsi => Jsi.run(self),
+            AbiFlavor::Wasm => Wasm.run(self),
         }
     }
 }
