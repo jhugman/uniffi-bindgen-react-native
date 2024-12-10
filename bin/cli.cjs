e937ddafd932d179e06c91ed5b703e3c59dfb8c6
Daniel Salinas <danielsalinas@daniels-mbp-2.myfiosgateway.com>
2024-12-10 09:58:59 -0500

    Fix two path calls to use .cjs extension

--- bin/cli.cjs
+++ bin/cli.cjs
@@ -33,7 +33,7 @@ const rootDir = getRootDir();
 
 const args = process.argv.slice(2);
 if (args.length === 1 && args[0] === "--path") {
-  console.log(`${rootDir}/bin/cli`);
+  console.log(`${rootDir}/bin/cli.cjs`);
   process.exit(0);
 }
 
