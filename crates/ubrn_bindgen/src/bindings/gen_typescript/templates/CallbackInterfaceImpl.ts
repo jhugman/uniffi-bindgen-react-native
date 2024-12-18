1e38b05d6945122ee4f7a1174212869becd975e8
James Hugman <james@hugman.tv>
2024-12-18 16:57:30 +0000

    Switch from askama to rinja

--- crates/ubrn_bindgen/src/bindings/gen_typescript/templates/CallbackInterfaceImpl.ts
+++ crates/ubrn_bindgen/src/bindings/gen_typescript/templates/CallbackInterfaceImpl.ts
@@ -27,7 +27,7 @@ const {{ trait_impl }}: { vtable: {{ vtable|ffi_type_name }}; register: () => vo
             {%- endif %}
             : {% call ts::return_type(meth) %} => {
                 const jsCallback = {{ ffi_converter_name }}.lift(uniffiHandle);
-                return {% call ts::await(meth) %}jsCallback.{{ meth.name()|fn_name }}(
+                return {% call ts::await_kw(meth) %}jsCallback.{{ meth.name()|fn_name }}(
                     {%- for arg in meth.arguments() %}
                     {{ arg|ffi_converter_name(self) }}.lift({{ arg.name()|var_name }}){% if !loop.last %}, {% endif %}
                     {%- endfor %}
