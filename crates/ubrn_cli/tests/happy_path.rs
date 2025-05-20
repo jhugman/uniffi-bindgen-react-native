53012c162c1e07be5d492e252d3cb742fb570565
James Hugman <james@hugman.tv>
2025-05-20 16:23:25 +0100

    Testing happy path cli for ios and android

--- crates/ubrn_cli/tests/happy_path.rs
+++ crates/ubrn_cli/tests/happy_path.rs
@@ -11,8 +11,8 @@ use camino::Utf8PathBuf;
 use clap::Parser;
 
 use ubrn_cli::cli;
-use ubrn_cli_testing::{assert_commands, shim_path, with_fixture, Command};
-use ubrn_common::run_cmd;
+use ubrn_cli_testing::{assert_commands, assert_files, shim_path, with_fixture, Command, File};
+use ubrn_common::{run_cmd, CrateMetadata};
 
 fn root_dir() -> Utf8PathBuf {
     Utf8PathBuf::from(env!("CARGO_MANIFEST_DIR"))
@@ -31,7 +31,7 @@ fn run_cli(command_line: impl Display) -> Result<()> {
     args.run()
 }
 
-fn cargo_buld(fixture_name: &str, target: &str) -> Result<Utf8PathBuf> {
+fn cargo_buld(fixture_name: &str, target: &str) -> Result<CrateMetadata> {
     let mut command = std::process::Command::new("cargo");
     let fixture = integration_fixtures_dir().join(fixture_name);
     let manifest_path = fixture.join("Cargo.toml");
@@ -39,15 +39,16 @@ fn cargo_buld(fixture_name: &str, target: &str) -> Result<Utf8PathBuf> {
         command
             .arg("build")
             .arg("--manifest-path")
-            .arg(manifest_path)
+            .arg(&manifest_path)
             .args(["--target", target]),
     )?;
-    Ok(fixture)
+    CrateMetadata::try_from(manifest_path)
 }
 
 #[test]
-fn test_build_command() -> Result<()> {
-    let rust_fixture = cargo_buld("arithmetic", "aarch64-apple-ios")?;
+fn test_build_command_ios() -> Result<()> {
+    let target = "aarch64-apple-ios";
+    let target_crate = cargo_buld("arithmetic", target)?;
     let fixtures_dir = fixtures_dir();
     with_fixture(fixtures_dir.clone(), "happy-path", |_fixture_dir| {
         // Set up file shims
@@ -56,15 +57,182 @@ fn test_build_command() -> Result<()> {
             "ubrn.config.yaml",
             fixtures_dir.join("defaults/ubrn.config.yaml"),
         );
-        shim_path("rust/shim/Cargo.toml", rust_fixture.join("Cargo.toml"));
-        shim_path("rust/shim", rust_fixture);
+        shim_path("rust/shim/Cargo.toml", target_crate.manifest_path());
+        shim_path("rust/shim", target_crate.project_root());
+        shim_path(
+            "libarithmetical.a",
+            target_crate.library_path(Some(target), "debug"),
+        );
 
         // Run the command under test
         run_cli("ubrn build ios --and-generate --config ubrn.config.yaml")?;
 
         // Assert the expected commands were executed
         assert_commands(&[
-            Command::new("xcodebuild").arg_pair_suffix("-xcframework", ".xcframework")
+            Command::new("cargo")
+                .arg("build")
+                .arg_pair_suffix("--manifest-path", "arithmetic/Cargo.toml")
+                .arg_pair("--target", "aarch64-apple-ios"),
+            Command::new("cargo")
+                .arg("build")
+                .arg_pair_suffix("--manifest-path", "arithmetic/Cargo.toml")
+                .arg_pair("--target", "aarch64-apple-ios-sim"),
+            Command::new("xcodebuild")
+                .arg("-create-xcframework")
+                .arg_pair_suffix("-library", "aarch64-apple-ios/debug/libarithmetical.a")
+                .arg_pair_suffix("-library", "aarch64-apple-ios-sim/debug/libarithmetical.a")
+                .arg_pair_suffix("-output", "DefaultFixtureFramework.xcframework"),
+        ]);
+
+        assert_files(&[
+            // This is the entry point to the module. It calls into the generated installRustCrate()
+            // method.
+            File::new("src/index.tsx")
+                .contains("installer.installRustCrate()")
+                .contains("import * as arithmetic from './generated/arithmetic'")
+                .contains("export * from './generated/arithmetic'")
+                .contains("arithmetic.default.initialize()"),
+            // This is the file that specifies installRustCrate method.
+            // It is the entry point to the RN codegen.
+            File::new("src/NativeDefaultFixture.ts").contains(
+                "export default TurboModuleRegistry.getEnforcing<Spec>('DefaultFixture')",
+            ),
+            // These next two files hook into the RN codegen generated files
+            File::new("ios/DefaultFixture.h")
+                .contains("#import \"DefaultFixtureSpec.h\"")
+                .contains("@interface DefaultFixture : NSObject <NativeDefaultFixtureSpec>"),
+            File::new("ios/DefaultFixture.mm")
+                .contains("#import \"DefaultFixture.h\"")
+                .contains("__hostFunction_DefaultFixture_installRustCrate")
+                .contains("NativeDefaultFixtureSpecJS")
+                .contains("defaultfixture::installRustCrate"),
+            // These next two are the cross platform entrypoint to the bindings.
+            File::new("cpp/default-fixture.h").contains("namespace defaultfixture"),
+            File::new("cpp/default-fixture.cpp")
+                .contains("namespace defaultfixture")
+                .contains("#include \"default-fixture.h\"")
+                .contains("#include \"generated/arithmetic.hpp\"")
+                .contains("NativeArithmetic::registerModule"),
+            // Finally, the bindings. We have tested the content of these in other tests.
+            File::new("src/generated/arithmetic.ts"),
+            File::new("src/generated/arithmetic-ffi.ts"),
+            File::new("cpp/generated/arithmetic.cpp"),
+            File::new("cpp/generated/arithmetic.hpp"),
+            // Assorted build files
+            // This is the podspec that tells iOS about the Rust framework generated with xcodebuild.
+            File::new("DefaultFixture.podspec")
+                .contains("s.vendored_frameworks = \"DefaultFixtureFramework.xcframework\""),
+        ]);
+
+        Ok(())
+    })
+}
+
+#[test]
+fn test_build_command_android() -> Result<()> {
+    let target = "aarch64-apple-ios";
+    let target_crate = cargo_buld("arithmetic", target)?;
+    let fixtures_dir = fixtures_dir();
+    with_fixture(fixtures_dir.clone(), "happy-path", |_fixture_dir| {
+        // Set up file shims
+        shim_path("package.json", fixtures_dir.join("defaults/package.json"));
+        shim_path(
+            "ubrn.config.yaml",
+            fixtures_dir.join("defaults/ubrn.config.yaml"),
+        );
+        shim_path("rust/shim/Cargo.toml", target_crate.manifest_path());
+        shim_path("rust/shim", target_crate.project_root());
+        shim_path(
+            "libarithmetical.a",
+            target_crate.library_path(Some(target), "debug"),
+        );
+
+        // Run the command under test
+        run_cli("ubrn build android --and-generate --config ubrn.config.yaml")?;
+
+        // Assert the expected commands were executed
+        assert_commands(&[
+            Command::new("cargo")
+                .arg("ndk")
+                .arg_pair_suffix("--manifest-path", "fixtures/arithmetic/Cargo.toml")
+                .arg_pair_suffix("--target", "arm64-v8a")
+                .arg_pair("--platform", "23")
+                .arg("--no-strip")
+                .arg("--")
+                .arg("build"),
+            Command::new("cargo")
+                .arg("ndk")
+                .arg_pair_suffix("--manifest-path", "fixtures/arithmetic/Cargo.toml")
+                .arg_pair_suffix("--target", "armeabi-v7a")
+                .arg_pair("--platform", "23")
+                .arg("--no-strip")
+                .arg("--")
+                .arg("build"),
+            Command::new("cargo")
+                .arg("ndk")
+                .arg_pair_suffix("--manifest-path", "fixtures/arithmetic/Cargo.toml")
+                .arg_pair_suffix("--target", "x86")
+                .arg_pair("--platform", "23")
+                .arg("--no-strip")
+                .arg("--")
+                .arg("build"),
+            Command::new("cargo")
+                .arg("ndk")
+                .arg_pair_suffix("--manifest-path", "fixtures/arithmetic/Cargo.toml")
+                .arg_pair_suffix("--target", "x86_64")
+                .arg_pair("--platform", "23")
+                .arg("--no-strip")
+                .arg("--")
+                .arg("build"),
+        ]);
+
+        assert_files(&[
+            // This is the entry point to the module. It calls into the generated installRustCrate()
+            // method.
+            File::new("src/index.tsx")
+                .contains("installer.installRustCrate()")
+                .contains("import * as arithmetic from './generated/arithmetic'")
+                .contains("export * from './generated/arithmetic'")
+                .contains("arithmetic.default.initialize()"),
+            // This is the file that specifies installRustCrate method.
+            // It is the entry point to the RN codegen.
+            File::new("src/NativeDefaultFixture.ts").contains(
+                "export default TurboModuleRegistry.getEnforcing<Spec>('DefaultFixture')",
+            ),
+            // // These next two files hook into the RN codegen generated files
+            File::new("android/src/main/java/com/defaultfixture/DefaultFixturePackage.kt")
+                .contains("class DefaultFixturePackage")
+                .contains("DefaultFixtureModule.NAME"),
+            File::new("android/src/main/java/com/defaultfixture/DefaultFixtureModule.kt")
+                .contains("class DefaultFixtureModule")
+                .contains("System.loadLibrary(\"default-fixture\")"),
+            File::new("android/build.gradle")
+                .contains("jsRootDir = file(\"../src/\")")
+                .contains("libraryName = \"DefaultFixture\"")
+                .contains("codegenJavaPackageName = \"com.defaultfixture\""),
+            File::new("android/CMakeLists.txt")
+                .contains("../cpp/default-fixture.cpp")
+                .contains("../cpp/generated/arithmetic.cpp")
+                .contains("cpp-adapter.cpp"),
+            File::new("android/cpp-adapter.cpp")
+                .contains("Java_com_defaultfixture_DefaultFixtureModule_nativeInstallRustCrate")
+                .contains("defaultfixture::installRustCrate"),
+            // These next two are the cross platform entrypoint to the bindings.
+            File::new("cpp/default-fixture.h").contains("namespace defaultfixture"),
+            File::new("cpp/default-fixture.cpp")
+                .contains("namespace defaultfixture")
+                .contains("#include \"default-fixture.h\"")
+                .contains("#include \"generated/arithmetic.hpp\"")
+                .contains("NativeArithmetic::registerModule"),
+            // Finally, the bindings. We have tested the content of these in other tests.
+            File::new("src/generated/arithmetic.ts"),
+            File::new("src/generated/arithmetic-ffi.ts"),
+            File::new("cpp/generated/arithmetic.cpp"),
+            File::new("cpp/generated/arithmetic.hpp"),
+            // Assorted glue and build files.
+            // This is the AndroidManifest which tells the android app about this package.
+            File::new("android/src/main/AndroidManifest.xml")
+                .contains("package=\"com.defaultfixture\""),
         ]);
 
         Ok(())
