6b759bbf6df7257229d375d1dc78f6859677b040
Nasr <larko236@outlook.com>
2025-12-12 13:08:00 +0100

    [This is work in progress, taken from https://github.com/jhugman/uniffi-bindgen-react-native/pull/322]

--- crates/ubrn_bindgen/src/bindings/gen_typescript/filters.rs
+++ crates/ubrn_bindgen/src/bindings/gen_typescript/filters.rs
@@ -7,10 +7,8 @@ use super::{
     oracle::{AsCodeType, CodeOracle},
     TypeRenderer,
 };
-pub(crate) use uniffi_bindgen::backend::filters::*;
 use uniffi_bindgen::{
-    backend::{Literal, Type},
-    interface::{AsType, Enum, FfiType, Variant},
+    interface::{AsType, Enum, FfiType, Literal, Type, Variant},
     ComponentInterface,
 };
 
@@ -101,11 +99,34 @@ pub(super) fn lift_fn(
 }
 
 pub fn render_literal(
-    literal: &Literal,
+    literal: &uniffi_meta::DefaultValueMetadata,
     as_ct: &impl AsType,
     ci: &ComponentInterface,
 ) -> Result<String, askama::Error> {
-    Ok(as_ct.as_codetype().literal(literal, ci))
+    // In UniFFI 0.30, the default value system changed to use DefaultValueMetadata
+    let type_ = as_ct.as_type();
+    let code_type = type_.as_codetype();
+
+    Ok(match literal {
+        uniffi_meta::DefaultValueMetadata::Literal(lit) => {
+            // Use the actual literal value
+            code_type.literal(lit, ci)
+        }
+        uniffi_meta::DefaultValueMetadata::Default => {
+            // Generate sensible defaults based on type
+            match type_ {
+                Type::String => "\"\"".to_string(),
+                Type::Boolean => "false".to_string(),
+                Type::Int8 | Type::Int16 | Type::Int32 => "0".to_string(),
+                Type::Int64 => "0n".to_string(),
+                Type::UInt8 | Type::UInt16 | Type::UInt32 => "0".to_string(),
+                Type::UInt64 => "0n".to_string(),
+                Type::Float32 | Type::Float64 => "0.0".to_string(),
+                Type::Optional { .. } => "undefined".to_string(),
+                _ => format!("/* default for {} */", code_type.type_label(ci)),
+            }
+        }
+    })
 }
 
 pub fn variant_discr_literal(
@@ -193,6 +214,11 @@ pub fn ffi_callback_name(nm: &str) -> Result<String, askama::Error> {
 /// Get the idiomatic Typescript rendering of an FFI struct name
 pub fn ffi_struct_name(nm: &str) -> Result<String, askama::Error> {
     Ok(CodeOracle.ffi_struct_name(nm))
+}
+
+/// Convert a Type to its corresponding FfiType
+pub fn ffi_type(type_: &Type) -> Result<FfiType, askama::Error> {
+    Ok(FfiType::from(type_))
 }
 
 /// Get the idiomatic Typescript rendering of docstring
--- crates/ubrn_bindgen/src/bindings/gen_typescript/oracle.rs
+++ crates/ubrn_bindgen/src/bindings/gen_typescript/oracle.rs
@@ -5,8 +5,7 @@
  */
 use heck::{ToLowerCamelCase, ToUpperCamelCase};
 use uniffi_bindgen::{
-    backend::{Literal, Type},
-    interface::{AsType, FfiType},
+    interface::{AsType, FfiType, Literal, Type},
     ComponentInterface,
 };
 
@@ -69,7 +68,7 @@ impl CodeOracle {
             FfiType::UInt64 | FfiType::Int64 => "0n".to_owned(),
             FfiType::Float64 => "0.0".to_owned(),
             FfiType::Float32 => "0.0".to_owned(),
-            FfiType::RustArcPtr(_) => "null".to_owned(),
+            FfiType::Handle => "0n".to_owned(),
             FfiType::RustBuffer(_) => "/*empty*/ new Uint8Array(0)".to_owned(),
             FfiType::Callback(_) => "null".to_owned(),
             FfiType::RustCallStatus => "uniffiCreateCallStatus()".to_owned(),
@@ -96,7 +95,7 @@ impl CodeOracle {
                 self.ffi_type_label(ffi_type)
             }
             FfiType::Struct(_) => self.ffi_type_label(ffi_type),
-            FfiType::RustArcPtr(_) => "PointerByReference".to_owned(),
+            FfiType::Handle => "bigint".to_owned(),
             // JNA structs default to ByReference
             _ => panic!("{ffi_type:?} by reference is not implemented"),
         }
@@ -104,7 +103,7 @@ impl CodeOracle {
 
     pub(crate) fn ffi_type_label_for_cpp(&self, ffi_type: &FfiType) -> String {
         match ffi_type {
-            FfiType::RustArcPtr(_) => "UniffiRustArcPtr".to_string(),
+            FfiType::Handle => "uint64_t".to_string(),
             FfiType::ForeignBytes => "Uint8Array".to_string(),
             FfiType::RustBuffer(_) => "string".to_string(),
             _ => self.ffi_type_label(ffi_type),
@@ -120,7 +119,6 @@ impl CodeOracle {
             FfiType::Float32 => "number".to_string(),
             FfiType::Float64 => "number".to_string(),
             FfiType::Handle => "bigint".to_string(),
-            FfiType::RustArcPtr(_) => "bigint".to_string(),
             FfiType::RustBuffer(_) => "Uint8Array".to_string(),
             FfiType::RustCallStatus => "UniffiRustCallStatus".to_string(),
             FfiType::ForeignBytes => "ForeignBytes".to_string(),
