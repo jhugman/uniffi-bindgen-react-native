95f6bbd0c8228c945ef81877b19a386955dd5aca
James Hugman <james@hugman.tv>
2026-02-23 14:59:53 +0000

    Remove copy/paste python test

--- fixtures/ext-types/subcrates/custom-types/tests/test_generated_bindings.rs
+++ fixtures/ext-types/subcrates/custom-types/tests/test_generated_bindings.rs
@@ -3,4 +3,3 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/
  */
-uniffi::build_foreign_language_testcases!("tests/bindings/test_guid.py",);
