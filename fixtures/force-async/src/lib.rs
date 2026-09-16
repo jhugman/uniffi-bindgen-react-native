2e2e47896e339ec88bd207fddc1608431c8662a5
James Hugman <james@hugman.tv>
2026-09-16 20:09:09 +0100

    test(fixtures): force-async runs on AsyncWasm

--- fixtures/force-async/src/lib.rs
+++ fixtures/force-async/src/lib.rs
@@ -3,6 +3,10 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/
  */
+
+#[cfg(target_arch = "wasm32")]
+extern crate uniffi_runtime_wasm as _;
+
 use std::sync::Arc;
 
 // Object with a primary constructor, a plain method, and all five traits.
